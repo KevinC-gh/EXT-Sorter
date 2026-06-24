935c09c1956f9736ed8eee4b089876e013cb91ee
Sussy_Hyena <kevin.currier5@gmail.com>
2026-06-23 19:53:05 -0600

    J: Backend added for multiple file type entries; program will now properly move multiple file types at once.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     
     root = tk.Tk()
     root.title("Sort by Extension")
-    root.geometry("1000x200")
+    root.geometry("1000x500")
 
     source_folder_entry = ttk.Entry(root, width=100)
     source_folder_entry.grid(row=0, column=1, padx=20, pady=5, sticky="w")
@@ -21,16 +21,16 @@ def main():
     dest_folder_button = ttk.Button(root, text="Add Destination Folder", command=lambda: add_dest_folder(dest_folder_entry))
     dest_folder_button.grid(row=1, column=0, padx=20, pady=5, sticky="w")
 
-    dropdown_entry = ttk.Entry(root, width=50)
-    dropdown_entry.grid(row=2, column=1, padx=20, pady=5, sticky="w")
+    dropdown_textbox = tk.Text(root, width=8, height=15)
+    dropdown_textbox.grid(row=2, column=1, padx=20, pady=5, sticky="w")
 
     options = [".png", ".jpg", ".docx", ".pdf", ".txt"]
     dropdown = ttk.Combobox(root, width=16, values=options, state="readonly")
-    dropdown.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+    dropdown.grid(row=2, column=0, padx=20, pady=5, sticky="nw")
     dropdown.set("Select File Type(s)")
-    dropdown.bind("<<ComboboxSelected>>", lambda event: list_selected(event, dropdown, dropdown_entry))
+    dropdown.bind("<<ComboboxSelected>>", lambda event: list_selected(event, dropdown, dropdown_textbox))
 
-    run_button = ttk.Button(root, text="Run", command=lambda: run(dropdown, source_folder_entry, dest_folder_entry))
+    run_button = ttk.Button(root, text="Run", command=lambda: run(dropdown_textbox, source_folder_entry, dest_folder_entry))
     run_button.grid(row=3, column=0, padx=20, pady=10, sticky="w")
 
     root.mainloop()
@@ -43,22 +43,24 @@ def add_dest_folder(dest_folder_entry):
     folder_path = filedialog.askdirectory(initialdir="/mnt/c/users", title="Select Folder")
     dest_folder_entry.insert(0, folder_path)
 
-def list_selected(_, dropdown, dropdown_entry):
+def list_selected(_, dropdown, dropdown_textbox):
     dropdown_selection = dropdown.get()
-    dropdown_entry.insert(tk.END, dropdown_selection)
+    dropdown_textbox.insert(tk.END, f"{dropdown_selection}\n")
     dropdown.set("Select File Type(s)")
 
-def run(dropdown, source_folder_entry, dest_folder_entry):
-    file_type_selection = dropdown.get()
+def run(dropdown_textbox, source_folder_entry, dest_folder_entry):
+    file_type_selections = dropdown_textbox.get("1.0","end-2c")
     source_folder = source_folder_entry.get()
     dest_folder = dest_folder_entry.get()
-    
+    selections_list = file_type_selections.split("\n")
     source_files = os.listdir(source_folder)
-    for file in source_files:
-        if file.endswith(file_type_selection):
-            full_source_path = os.path.abspath(os.path.join(source_folder, file))
-            full_dest_path = os.path.abspath(os.path.join(dest_folder, file))
-            os.rename(full_source_path, full_dest_path)
-            print(f"{file} moved successfully")
+    
+    for file_type in selections_list:
+        for file in source_files:
+            if file.endswith(file_type):
+                full_source_path = os.path.abspath(os.path.join(source_folder, file))
+                full_dest_path = os.path.abspath(os.path.join(dest_folder, file))
+                os.rename(full_source_path, full_dest_path)
+                print(f"{file} moved successfully")
 
 main()
