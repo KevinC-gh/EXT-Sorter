35e9c652c797344921fd8550f600021528a23269
Sussy_Hyena <kevin.currier5@gmail.com>
2026-06-26 10:54:32 -0600

    L: Options in dropdown disappear after being selected to prevent duplicate selections. Status box added to display error messages.

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ def main():
     root = tk.Tk()
     root.title("Sort by Extension")
     root.geometry("1100x400")
+    bg_color = "LightBlue2"
+    root.configure(bg=bg_color)
 
     source_folder_entry = ttk.Entry(root, width=100)
     source_folder_entry.grid(row=0, column=1, padx=20, pady=5, sticky="w")
-    source_folder_button = ttk.Button(root, text="Add Source Folder", command=lambda: add_source_folder(source_folder_entry))
+    source_folder_button = ttk.Button(root, text="Add Source Folder", 
+                                      command=lambda: add_source_folder(source_folder_entry))
     source_folder_button.grid(row=0, column=0, padx=20, pady=5, sticky="w")
 
     dest_folder_entry = ttk.Entry(root, width=100)
     dest_folder_entry.grid(row=1, column=1, padx=20, pady=5, sticky="w")
-    dest_folder_button = ttk.Button(root, text="Add Destination Folder", command=lambda: add_dest_folder(dest_folder_entry))
+    dest_folder_button = ttk.Button(root, text="Add Destination Folder", 
+                                    command=lambda: add_dest_folder(dest_folder_entry))
     dest_folder_button.grid(row=1, column=0, padx=20, pady=5, sticky="w")
 
     dropdown_textbox = tk.Text(root, width=6, height=1)
@@ -28,10 +32,17 @@ def main():
     dropdown = ttk.Combobox(root, width=16, values=options, state="readonly")
     dropdown.grid(row=2, column=0, padx=20, pady=5, sticky="nw")
     dropdown.set("Select File Type(s)")
-    dropdown.bind("<<ComboboxSelected>>", lambda event: list_selected(event, dropdown, dropdown_textbox))
+    dropdown.bind("<<ComboboxSelected>>", 
+                  lambda event: list_selected(event, dropdown, dropdown_textbox, options))
 
-    run_button = ttk.Button(root, text="Run", command=lambda: run(dropdown_textbox, source_folder_entry, dest_folder_entry))
+    run_button = ttk.Button(root, text="Run", 
+                            command=lambda: run(dropdown_textbox, source_folder_entry, dest_folder_entry))
     run_button.grid(row=3, column=1, padx=20, pady=10, sticky="nw")
+
+    status_box = tk.Label(root, width=100, state="disabled",
+                           bg=bg_color, relief="flat", bd=0, 
+                           highlightthickness=0)
+    status_box.grid(row=4, column=1, padx=20, pady=5, sticky="w")
 
     root.mainloop()
 
@@ -43,13 +54,20 @@ def add_dest_folder(dest_folder_entry):
     folder_path = filedialog.askdirectory(initialdir="/mnt/c/users", title="Select Folder")
     dest_folder_entry.insert(0, folder_path)
 
-def list_selected(_, dropdown, dropdown_textbox):
+def list_selected(_, dropdown, dropdown_textbox, options):
+    #display selection option, temporarily enable dropdown textbox edits to allow text to display
     dropdown_textbox.config(state="normal")
     dropdown_selection = dropdown.get()
     dropdown_textbox.insert("1.0", f"{dropdown_selection}\n")
     dropdown.set("Select File Type(s)")
+    
+    #expand dropdown textbox, disable ability for user edits to dropdown textbox
     new_height = dropdown_textbox.cget("height") + 1
     dropdown_textbox.config(height=new_height, state="disabled")
+    
+    #remove option from dropdown to prevent duplicates
+    options.remove(dropdown_selection)
+    dropdown.config(values=options)
 
 def run(dropdown_textbox, source_folder_entry, dest_folder_entry):
     file_type_selections = dropdown_textbox.get("1.0","end-2c")
